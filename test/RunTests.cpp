d817e653c08d558d218418eb8d0f3f703bb3bc45
Justin Wilson - jkw0002 <jkw0002@uah.edu>
2020-02-16 19:41:06 -0600

    Separated Test Definitions from test runner main()

---
+++ test/RunTests.cpp
@@ -0,0 +1,16 @@
+/*
+ * RunTests.cpp
+ *
+ *  Created on: Feb 14, 2020
+ *      Author: jwredhead
+ */
+#include <iostream>
+#include <gtest/gtest.h>
+#include "MatrixTest.h"
+
+
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+

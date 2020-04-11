9b0b7dc058f870337811a5b0652f8e0220e86c87
Justin Wilson - jkw0002 <jkw0002@uah.edu>
2020-04-11 13:52:05 -0500

    Added CUDA_CALL macro, added commented code prototyping

--- CUDA/src/cuda_extension.cu
+++ CUDA/src/cuda_extension.cu
@@ -6,6 +6,10 @@
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
 
+#define  CUDA_CALL(x) do { if((x) !=  cudaSuccess) { \
+	printf ("Error  at %s:%d\n",__FILE__ ,__LINE__); \
+	return  EXIT_FAILURE ;}}  while (0)
+
 namespace cuda_extension {
 
 cublasHandle_t handle;
@@ -16,6 +20,8 @@ inline int initDevice(IN_Layer inLayer, std::vector<NN_Layer> hiddenLayers, NN_L
 	cudaError_t err;
 	const char *str = "";
 
+	curandState *devStates;
+
 	// Get CUDA device
 	int dev = findCudaDevice(0, &str);
 	if (dev == -1) {
@@ -23,7 +29,7 @@ inline int initDevice(IN_Layer inLayer, std::vector<NN_Layer> hiddenLayers, NN_L
 	}
 
 	// Create CUBLAS Handle
-	printf("INITIALIZING CUBLASS....");
+	printf("INITIALIZING CUBLASS...");
 
 	status = cublasCreate(&handle);
 	if (status != CUBLAS_STATUS_SUCCESS) {
@@ -37,80 +43,50 @@ inline int initDevice(IN_Layer inLayer, std::vector<NN_Layer> hiddenLayers, NN_L
 	// Allocate Memory on device for layers
 	printf("ALLOCATING DEVICE MEMORY...");
 
-	err = cudaMalloc(reinterpret_cast<void**>(&(inLayer.inputs)), inLayer.Nodes * sizeof(float));
+	// Input Layer
+	CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(inLayer.inputs)), inLayer.Nodes * sizeof(float)));
 
-	if( err != cudaSuccess) {
-		fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE INPUT LAYER");
-		return EXIT_FAILURE;
-	}
+	// Output Layer
+	CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(outLayer.output)), outLayer.Nodes * sizeof(float)));
 
-	err = cudaMalloc(reinterpret_cast<void**>(&(outLayer.output)), outLayer.Nodes * sizeof(float));
+	CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(outLayer.bias)), outLayer.Nodes * sizeof(float)));
 
-	if( err != cudaSuccess) {
-		fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE OUTPUT LAYER");
-		return EXIT_FAILURE;
-	}
+	CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(outLayer.error)), outLayer.Nodes * sizeof(float)));
 
-	err = cudaMalloc(reinterpret_cast<void**>(&(outLayer.bias)), outLayer.Nodes * sizeof(float));
+	CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(outLayer.weights)), *outLayer.wRows * outLayer.wCols * sizeof(float)));
 
-	if( err != cudaSuccess) {
-		fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE OUTPUT LAYER");
-		return EXIT_FAILURE;
-	}
-
-	err = cudaMalloc(reinterpret_cast<void**>(&(outLayer.error)), outLayer.Nodes * sizeof(float));
-
-	if( err != cudaSuccess) {
-		fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE OUTPUT LAYER");
-		return EXIT_FAILURE;
-	}
-
-	err = cudaMalloc(reinterpret_cast<void**>(&(outLayer.weights)), *outLayer.wRows * outLayer.wCols * sizeof(float));
-
-	if( err != cudaSuccess) {
-		fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE OUTPUT LAYER");
-		return EXIT_FAILURE;
-	}
-
-
+	// Hidden Layers
 	for (auto i : hiddenLayers) {
-		err = cudaMalloc(reinterpret_cast<void**>(&(i.output)), i.Nodes * sizeof(float));
+		CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(i.output)), i.Nodes * sizeof(float)));
 
-		if( err != cudaSuccess) {
-			fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE HIDDEN LAYER %d", i);
-			return EXIT_FAILURE;
-		}
+		CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(i.bias)), i.Nodes * sizeof(float)));
 
-		err = cudaMalloc(reinterpret_cast<void**>(&(i.bias)), i.Nodes * sizeof(float));
+		CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(i.error)), i.Nodes * sizeof(float)));
 
-		if( err != cudaSuccess) {
-			fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE OUTPUT LAYER");
-			return EXIT_FAILURE;
-		}
+		CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&(i.weights)), *i.wRows * i.wCols * sizeof(float)));
 
-		err = cudaMalloc(reinterpret_cast<void**>(&(i.error)), i.Nodes * sizeof(float));
-
-		if( err != cudaSuccess) {
-			fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE OUTPUT LAYER");
-			return EXIT_FAILURE;
-		}
-
-		err = cudaMalloc(reinterpret_cast<void**>(&(i.weights)), *i.wRows * i.wCols * sizeof(float));
-
-		if( err != cudaSuccess) {
-			fprintf(stderr, "DEVICE MEMORY ALLOCATION ERROR: UNABLE TO ALLOCATE OUTPUT LAYER");
-			return EXIT_FAILURE;
-		}
 	}
 
 	printf("MEMORY ALLOCATED!");
 
+//	// Fill device weight and bias matrices with random data with CURAND
+//	printf("FILLING WEIGHTS AND BIAS WITH RANDOM VALUES...");
+//
+//	printf("INITIALIZING CURAND...");
+//
+//	CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&devStates), sizeof(curandState)));
+//
+//
 	return 0;
-
 
 
 }
 
+//__global__
+//void deviceRandomFill (float *matrix, unsigned size, curandState *globalState) {
+//	curandState localState;
+//	localState = global_State[]
+//}
 
 
 
